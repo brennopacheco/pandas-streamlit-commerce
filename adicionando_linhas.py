9cfba06baa22c875c099283f15e60d62e9631ab6
Brenno Pacheco <brenno_pacheco@hotmail.com>
2025-08-29 23:13:49 -0300

    adicionando coluna cidade/estado

--- adicionando_linhas.py
+++ adicionando_linhas.py
@@ -2,6 +2,12 @@ from datetime import datetime
 import streamlit as st
 import pandas as pd
 
-caminho_compras = 'datasets/compras.csv'
-df_compras = pd.read_csv(caminho_compras, decimal=',', sep=';') 
+caminho_datasets = 'datasets'
 
+df_compras = pd.read_csv(f'{caminho_datasets}/compras.csv', decimal=',', sep=';') 
+df_lojas = pd.read_csv(f'{caminho_datasets}/lojas.csv', decimal=',', sep=';')
+df_produtos = pd.read_csv(f'{caminho_datasets}/produtos.csv', decimal=',', sep=';')
+
+df_lojas['cidade/estado'] = df_lojas['cidade'] + '/' + df_lojas['estado'] # criando nova coluna
+lista_lojas = df_lojas['cidade/estado'].to_list() # convertendo para lista
+st.selectbox('Seleciona a Loja', lista_lojas)
