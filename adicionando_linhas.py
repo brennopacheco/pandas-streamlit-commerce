4f840a7ed9db64befcf3e3e32d325ec29db1d08c
Brenno Pacheco <brenno_pacheco@hotmail.com>
2025-09-09 23:31:55 -0300

    aplicacao web funcionando

--- adicionando_linhas.py
+++ adicionando_linhas.py
@@ -10,4 +10,37 @@ df_produtos = pd.read_csv(f'{caminho_datasets}/produtos.csv', decimal=',', sep='
 
 df_lojas['cidade/estado'] = df_lojas['cidade'] + '/' + df_lojas['estado'] # criando nova coluna
 lista_lojas = df_lojas['cidade/estado'].to_list() # convertendo para lista
-st.selectbox('Seleciona a Loja', lista_lojas)
+loja_selecionada = st.sidebar.selectbox('Seleciona a Loja', lista_lojas)
+
+lista_vendedores= df_lojas.loc[df_lojas['cidade/estado'] == loja_selecionada, 'vendedores'].iloc[0]
+lista_vendedores = lista_vendedores.strip('][').replace("'", '').split(", ")
+vendedor_selecionado = st.sidebar.selectbox("Selecione o vendedor:", lista_vendedores)
+
+lista_produtos = df_produtos['nome'].to_list()
+produto_selecionado = st.sidebar.selectbox('Selecione o produto:', lista_produtos)
+
+nome_cliente = st.sidebar.text_input('Nome do Cliente')
+genero_selecionado = st.sidebar.selectbox('Gênero do Clinte', ['Masculino', 'Femenino'])
+
+lista_pagamento = df_compras['forma_pagamento'].unique()
+forma_pagamento_selecionado = st.sidebar.selectbox('Selecione a forma de pagamento', lista_pagamento)
+
+
+if st.sidebar.button('Adicionar Nova Compra'):
+    lista_adicionar = [datetime.now().strftime('%Y-%m-%d %H:%M:%S'),  # coluna 'data',
+                       df_compras['id_compra'].max() + 1 if not df_compras.empty else 1,
+                       loja_selecionada,
+                       vendedor_selecionado,
+                       produto_selecionado,
+                       nome_cliente,
+                       genero_selecionado,
+                       forma_pagamento_selecionado]
+    df_compras.loc[len(df_compras)] = lista_adicionar   # adiciona no fim
+
+
+    df_compras.to_csv(f'{caminho_datasets}/compras.csv', decimal=',', sep=';', index=False)
+
+    st.success('Compra Adicionada')
+
+
+st.dataframe(df_compras)
