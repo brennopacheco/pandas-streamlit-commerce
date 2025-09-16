d57ed364d5fb2acd326551fef5a9ac622ca2b40b
Brenno Pacheco <brenno_pacheco@hotmail.com>
2025-09-16 19:09:49 -0300

    finalizando código

--- tabela_dinamica.py
+++ tabela_dinamica.py
@@ -15,11 +15,11 @@ df_lojas = pd.read_csv(f'{caminho_datasets}/lojas.csv', decimal=',', sep=';', in
 df_produtos = pd.read_csv(f'{caminho_datasets}/produtos.csv', decimal=',', sep=';', index_col=0)
 
 df_produtos = df_produtos.rename(columns={'nome': 'produto'})
-df_produtos.reset_index()
+df_compras = df_compras.reset_index()
 
-df_produtos = pd.merge(
-    left=df_produtos,
-    right=df_compras[['produto', 'preco']],
+df_compras = pd.merge(
+    left=df_compras,
+    right=df_produtos[['produto', 'preco']],
     on='produto',
     how='left'
 )
@@ -27,3 +27,27 @@ df_produtos = pd.merge(
 df_compras = df_compras.set_index('data')
 df_compras['comissao'] = df_compras['preco'] * PERCENTUAL_COMISSAO
 
+indice_dinamico = st.sidebar.multiselect('Selecione os índices', COLUNAS_ANALISE)
+
+colunas_filtradas = [c for c in COLUNAS_ANALISE if not c in indice_dinamico]
+
+coluna_dinamica = st.sidebar.multiselect('Selecione as colunas', colunas_filtradas)
+
+valor_analise = st.sidebar.selectbox('Selecione o valor', COLUNAS_NUMERICAS)
+
+metricas_analise = st.sidebar.selectbox('Selecionar a métrica', list(FUNCOES_AGREGACAO.keys()))
+
+if len(indice_dinamico) > 0 and len(coluna_dinamica) > 0:
+    metrica = FUNCOES_AGREGACAO[metricas_analise]
+    compras_dinamica = pd.pivot_table(
+        df_compras,
+        index=indice_dinamico,
+        columns=coluna_dinamica,
+        values=valor_analise,
+        aggfunc=metrica
+    )
+
+    compras_dinamica['TOTAL_GERAL'] = compras_dinamica.sum(axis=1)
+    compras_dinamica.loc['TOTAL_GERAL'] = compras_dinamica.sum(axis=0).to_list()
+
+    st.dataframe(compras_dinamica)
